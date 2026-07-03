2a662fb6b41c363bb97bf37460ee174d819b80b6
Prithbiraj Sarker <122400527+prithbi07@users.noreply.github.com>
2026-07-04 00:54:35 +0600

    Update data extraction parameters in 01_extract script

--- scripts/01_extract.py
+++ scripts/01_extract.py
@@ -14,7 +14,7 @@ if not os.path.exists(SAVE_DIR): os.makedirs(SAVE_DIR)
 # Initialize pose tracking from explicit solution path
 pose = mp_pose.Pose(static_image_mode=False, model_complexity=2, min_detection_confidence=0.5)
 
-def calculate_angle(a, b, c):
+def Calculate_Angle(a, b, c):
     a, b, c = np.array(a), np.array(b), np.array(c)
     radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
     return np.abs(radians*180.0/np.pi)
@@ -35,9 +35,9 @@ while cap.isOpened():
         hip = [lm[23].x, lm[23].y]
         
         # --- 5 FEATURE GEOMETRY ---
-        f1 = calculate_angle([ear[0], 0], sh, ear)
-        f2 = calculate_angle(ear, sh, hip) 
-        f3 = calculate_angle([sh[0], 0], hip, sh)
+        f1 = Calculate_Angle([ear[0], 0], sh, ear)
+        f2 = Calculate_Angle(ear, sh, hip) 
+        f3 = Calculate_Angle([sh[0], 0], hip, sh)
         torso_len = np.linalg.norm(np.array(sh) - np.array(hip)) + 1e-6
         f4 = (ear[0] - sh[0]) / torso_len
         f5 = (sh[1] - ear[1]) / torso_len
